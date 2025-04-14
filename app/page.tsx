e9cb4289aed666e97a5afe15fbbd56d985b85389
huzaifa iqbal <huzaifaiqbal5554@gmail.com>
2025-04-14 05:02:30 +0500

    favicon changes

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ import Footer from "@/components/Footer"; // Ensure case matches exactly
 export default function Home() {
   return (
     <>
+    
     <div className="main-cnt flex flex-col items-center relative">
       <div className="fixed top-0 left-0"> <Media /></div>
       <Header />
